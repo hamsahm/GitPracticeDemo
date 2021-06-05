6f257801e213ac6f946fa0f161cab3506d163d17
Hamsa <hamsahm28@gmail.com>
2021-06-05 14:50:01 +0530

    first commit for demo

--- src/HelloWorldExample.java
+++ src/HelloWorldExample.java
@@ -1,6 +1,7 @@
 
 public class HelloWorldExample {
 public static void main(String[] args) {
-	System.out.println("Hello World in GitPractice Demo");
+	System.out.println("Hello World in GitPractice Demo 1");
+	System.out.println("Hello World in GitPractice Demo 2");
 }
 }
